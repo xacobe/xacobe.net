f57241e79a704d4459edf36da18d4261957fa317
xacobe <xacobe.net@gmail.com>
2025-05-26 14:44:49 +0200

    update trabajos

---
+++ web/themes/custom/icosaedrico/js/index.js
@@ -0,0 +1,8 @@
+var elem = document.querySelector('.masonry-grid');
+var msnry = new Masonry( elem, {
+  // options
+  itemSelector: '.grid-item',
+  horizontalOrder: true,
+  columnWidth: 100
+});
+
