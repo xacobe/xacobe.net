8e643ecc750d4024f5cb7153e1a0dc4fb2091b3f
Xacobe M. Arostegui <x@xacobe.net>
2026-04-03 22:40:11 +0200

    storybook page update

---
+++ web/themes/custom/icosaedrico/.storybook/DocsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Title, Description, Primary, Controls } from '@storybook/addon-docs/blocks';
+
+export function DocsPage() {
+  return (
+    <>
+      <Title />
+      <Description />
+      <Primary />
+      <Controls />
+    </>
+  );
+}
--- web/themes/custom/icosaedrico/.storybook/preview.js
+++ web/themes/custom/icosaedrico/.storybook/preview.js
@@ -1,8 +1,13 @@
+import { DocsPage } from './DocsPage.jsx';
+
 /** @type { import('@storybook/server').Preview } */
 const preview = {
   parameters: {
     server: {
       url: `${process.env.STORYBOOK_DRUPAL_URL || 'https://xacobe.net.ddev.site'}/storybook/stories/render`,
+    },
+    docs: {
+      page: DocsPage,
     },
   },
 };
